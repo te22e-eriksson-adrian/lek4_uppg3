12b7f0f86cc6024e5cf4ed28348e247e048414ff
te22e-eriksson-adrian <adrian.eriksson@elev.ga.ntig.se>
2023-09-18 09:29:03 +0200

    la till kod som hämtar efternamn

--- src/App.java
+++ src/App.java
@@ -14,8 +14,17 @@ public class App {
         System.out.println("    ");
         int mellan1_index = text.indexOf(" ");
         System.out.println("1:a mellanslagets teckenposition: "+mellan1_index);
+        int mellan2_index = text.indexOf(" ",mellan1_index+1);
+        System.out.println("2:a mellanslagets teckenposition: "+mellan2_index);
+        int mellan3_index = text.indexOf(" ",mellan2_index+1);
+        System.out.println("3:e mellanslagets teckenposition: "+mellan3_index);
+        //int mellan4_index = text.indexOf(" ", mellan3_index+1);
+        //System.out.println("4:e mellanslagets teckenposition: "+mellan4_index);
         String förnamn = text.substring(0,mellan1_index);
         System.out.println("Förnamn: "+förnamn);
+        String Efternamn = text.substring(mellan1_index, mellan2_index);
+        System.out.println("Efternamn:"+Efternamn);
+        //double längd = text.substring(mellan3_index, mellan4_index);
         tangentbord.close();
     }
 }
