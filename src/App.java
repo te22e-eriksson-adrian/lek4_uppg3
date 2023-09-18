dde1cdecb7cbabbdcef0e8fa84092f2dfb7456bd
te22e-eriksson-adrian <adrian.eriksson@elev.ga.ntig.se>
2023-09-18 08:42:18 +0200

    la till kod som läser inmatningar från användaren

--- src/App.java
+++ src/App.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //Skriva in text.
+        Scanner tangentbord = new Scanner(System.in);
+        System.out.println("Instruktioner:");
+        System.out.println("Du ska nu fylla i dina uppgifter på följande sätt, skriv ditt förnamn, efternamn, din längd och din ålder på samma rad efter varandra med endast mellanslag mellan varandra.");
+        System.out.println("    ");
+        System.out.print("Fyll i dina uppgifter här: ");
+        String text = tangentbord.nextLine();
+        tangentbord.close();
     }
 }
