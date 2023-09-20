341db5445d5fa3e0f32061f38cf37361336cfea2
te22e-eriksson-adrian <adrian.eriksson@elev.ga.ntig.se>
2023-09-20 10:14:18 +0200

    uppg.del. a klar

--- src/App.java
+++ src/App.java
@@ -5,7 +5,7 @@ public class App {
         //Skriva in text.
         Scanner tangentbord = new Scanner(System.in);
         System.out.println("Instruktioner:");
-        System.out.println("Du ska nu fylla i dina uppgifter på följande sätt, skriv ditt förnamn, efternamn, din längd och din ålder på samma rad efter varandra med endast mellanslag mellan varandra.");
+        System.out.println("Du ska nu fylla i dina uppgifter på följande sätt, skriv ditt förnamn, efternamn, din längd och din ålder på samma rad efter varandra med endast mellanslag mellan varandra. Avsluta med ett till mellanslag innnan du trycker på ENTER.");
         System.out.println("    ");
         System.out.print("Fyll i dina uppgifter här: ");
         String text = tangentbord.nextLine();
@@ -18,13 +18,18 @@ public class App {
         System.out.println("2:a mellanslagets teckenposition: "+mellan2_index);
         int mellan3_index = text.indexOf(" ",mellan2_index+1);
         System.out.println("3:e mellanslagets teckenposition: "+mellan3_index);
-        //int mellan4_index = text.indexOf(" ", mellan3_index+1);
-        //System.out.println("4:e mellanslagets teckenposition: "+mellan4_index);
+        int mellan4_index = text.indexOf(" ", mellan3_index+1);
+        System.out.println("4:e mellanslagets teckenposition: "+mellan4_index);
         String förnamn = text.substring(0,mellan1_index);
         System.out.println("Förnamn: "+förnamn);
         String Efternamn = text.substring(mellan1_index, mellan2_index);
         System.out.println("Efternamn:"+Efternamn);
-        //double längd = text.substring(mellan3_index, mellan4_index);
+        String decimal_text = text.substring(mellan2_index, mellan3_index);
+        double längd = Double.parseDouble(decimal_text);
+        System.out.println("Längd:"+längd);
+        String siffra_text = text.substring(mellan3_index+1, mellan4_index);
+        int ålder = Integer.parseInt(siffra_text);
+        System.out.println("Ålder:"+ålder);
         tangentbord.close();
     }
 }
